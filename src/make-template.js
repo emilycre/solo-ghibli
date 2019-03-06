e2b93d5afd5998a35ff072f492e047ee4ecf912f
Emily Creamer <emily.creamer@icloud.com>
2019-03-05 17:24:13 -0800

    Created film template with passing test

---
+++ src/make-template.js
@@ -0,0 +1,8 @@
+export default function makeFilmTemplate(film) {
+    const html = `
+    <li><a href="films.html?id=${film.id}">${film.title}</a> <span>(${film.release_date})</span></li>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,24 @@
+import './html-equal.js';
+import makeFilmTemplate from '../src/make-template.js';
+
 const test = QUnit.test;
 
-QUnit.module('test suite one');
+QUnit.module('Film Listing');
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+
+test('Film list template matches HTML', function(assert) {
+    //arrange
+    const html = `
+    <li><a href="films.html">Castle In The Sky</a> <span>(1986)</span></li>`;
+    const film = {
+        title: 'Castle In The Sky',
+        release_date: '1986',
+        id: '123'
+    };
+    //act
+    const result = makeFilmTemplate(film);
+    
+    //assert
+    assert.htmlEqual(result, `
+    <li><a href="films.html?id=123">Castle In The Sky</a> <span>(1986)</span></li>`);
 });
