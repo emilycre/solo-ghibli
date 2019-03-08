ec92b28aecb15f49ddef69118a6176cc0754eb77
Emily Creamer <emily.creamer@icloud.com>
2019-03-08 09:06:40 -0800

    Finished CSS styling

--- src/film-detail-template.js
+++ src/film-detail-template.js
@@ -1,17 +1,17 @@
 export default function createDetailTemplate(film) {
     const html = `
     <dl>
-        <dt>Film Title</dt>
+        <dt>Film Title :</dt>
         <dd id="title">${film.title}</dd>
-        <dt>Synopsis</dt>
+        <dt>Synopsis :</dt>
         <dd id="synopsis">${film.description}</dd>
-        <dt>Director</dt>
+        <dt>Director :</dt>
         <dd id="director">${film.director}</dd>
-        <dt>Producer</dt>
+        <dt>Producer :</dt>
         <dd id="producer">${film.producer}</dd>
-        <dt>Release Date</dt>
+        <dt>Release Date :</dt>
         <dd id="release-date">${film.release_date}</dd>
-        <dt>Rotten Tomato Score</dt>
+        <dt>Rotten Tomato Score :</dt>
         <dd id="rt-score">${film.rt_score}</dd>
 
     </dl>
